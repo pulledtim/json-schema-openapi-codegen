5a79b5e1983074845ff789a771b7c84c086bac18
Tim Smyth <tim.smyth@fiware.org>
2023-07-11 15:35:03 +0200

    pipeline

--- src/main/java/org/openapitools/codegen/languages/SmartDataCodegen.java
+++ src/main/java/org/openapitools/codegen/languages/SmartDataCodegen.java
@@ -1,7 +1,5 @@
 package org.openapitools.codegen.languages;
 
-import io.micronaut.http.HttpStatus;
-import io.micronaut.http.MediaType;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
@@ -13,7 +11,6 @@ import org.openapitools.codegen.languages.features.UseGenericResponseFeatures;
 import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
 import org.openapitools.codegen.utils.ModelUtils;
-import org.openapitools.codegen.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,36 +25,19 @@ import static org.openapitools.codegen.CodegenConstants.*;
 public class SmartDataCodegen extends AbstractJavaCodegen
         implements BeanValidationFeatures, UseGenericResponseFeatures, OptionalFeatures {
 
-    public static final String CLIENT_ID = "clientId";
     public static final String INTROSPECTED = "introspected";
     public static final String DATETIME_RELAXED = "dateTimeRelaxed";
-    public static final String PAGEABLE = "pageable";
-    public static final String GENERATE_AUTHENTICATION = "generateAuthentication";
-    public static final String GENERATE_EXAMPLES = "generateExamples";
     public static final String SCHEMA_REGISTRY = "schemaRegistry";
 
-    // '{' or '}' is not allowed according to https://datatracker.ietf.org/doc/html/rfc6570#section-3.2
-    // so the RegExp needs to work around and be very verbose as quantifiers cannot be used.
-    private static final String UUID_PATTERN = "[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]"
-            + "-[a-f0-9][a-f0-9][a-f0-9][a-f0-9]"
-            + "-[a-f0-9][a-f0-9][a-f0-9][a-f0-9]"
-            + "-[a-f0-9][a-f0-9][a-f0-9][a-f0-9]"
-            + "-[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]";
     private static final Logger LOG = LoggerFactory.getLogger(SmartDataCodegen.class);
 
     private String schemaRegistry = "https://smart-data-models.github.io/data-models.test";
-    private boolean generateApiTests = true;
-    private boolean generateAuthentication = false;
-    private boolean generateExamples = false;
     private boolean useBeanValidation = true;
-    private boolean useGenericResponse = true;
     private boolean useOptional = true;
     private boolean introspected = true;
     private boolean dateTimeRelaxed = true;
-    private boolean pageable = false;
-    private boolean isClient = false;
 
-    private Map<String, String> fieldRenames = new HashMap<>();
+    private Map<String, String> fieldNameMapping = new HashMap<>();
 
     public SmartDataCodegen() {
 
@@ -68,20 +48,14 @@ public class SmartDataCodegen extends AbstractJavaCodegen
 
         cliOptions.clear();
         cliOptions.add(CliOption.newBoolean(USE_BEANVALIDATION, "Use bean validation annotations", useBeanValidation));
-        cliOptions.add(CliOption.newBoolean(USE_GENERIC_RESPONSE, "Use generic response", useGenericResponse));
         cliOptions.add(CliOption.newBoolean(USE_OPTIONAL, "Use Optional<T> instead of @Nullable.", useOptional));
         cliOptions.add(CliOption.newBoolean(INTROSPECTED, "Add @Introspected to models", introspected));
-        cliOptions.add(CliOption.newBoolean(SUPPORT_ASYNC, "Use async responses", supportAsync));
         cliOptions.add(CliOption.newBoolean(DATETIME_RELAXED, "Relaxed parsing of datetimes.", dateTimeRelaxed));
-        cliOptions.add(CliOption.newBoolean(PAGEABLE, "Generate provider for pageable (mironaut-data).", pageable));
         cliOptions.add(CliOption.newBoolean(OPENAPI_NULLABLE, "Enable OpenAPI Jackson Nullable", openApiNullable));
-        cliOptions.add(CliOption.newBoolean(GENERATE_AUTHENTICATION,
-                "Generate authentication into apis with return code 401.", generateAuthentication));
-        cliOptions.add(CliOption.newBoolean(GENERATE_EXAMPLES, "Generate examples for tests.", generateExamples));
-        cliOptions.add(CliOption.newString(CLIENT_ID, "ClientId to use."));
         cliOptions.add(CliOption.newString(SOURCE_FOLDER, SOURCE_FOLDER_DESC));
         cliOptions.add(CliOption.newString(SCHEMA_REGISTRY, "Url of the registry that will host the schemata"));
         cliOptions.add(CliOption.newString("testFolder", "test folder for generated code"));
+        cliOptions.add(CliOption.newString("fieldNameMapping", "Field names to be replaced"));
 
         // there is no documentation template yet
 
@@ -97,12 +71,8 @@ public class SmartDataCodegen extends AbstractJavaCodegen
 
         additionalProperties.clear();
         additionalProperties.put(USE_BEANVALIDATION, useBeanValidation);
-        additionalProperties.put(USE_GENERIC_RESPONSE, useGenericResponse);
         additionalProperties.put(USE_OPTIONAL, useOptional);
         additionalProperties.put(INTROSPECTED, introspected);
-        additionalProperties.put(PAGEABLE, pageable);
-        additionalProperties.put(GENERATE_AUTHENTICATION, generateAuthentication);
-        additionalProperties.put(GENERATE_EXAMPLES, generateExamples);
 
         // add custom type mappings
 
@@ -185,9 +155,6 @@ public class SmartDataCodegen extends AbstractJavaCodegen
         if (additionalProperties.containsKey(USE_BEANVALIDATION)) {
             useBeanValidation = convertPropertyToBooleanAndWriteBack(USE_BEANVALIDATION);
         }
-        if (additionalProperties.containsKey(USE_GENERIC_RESPONSE)) {
-            useGenericResponse = convertPropertyToBooleanAndWriteBack(USE_GENERIC_RESPONSE);
-        }
         if (additionalProperties.containsKey(USE_OPTIONAL)) {
             useOptional = convertPropertyToBooleanAndWriteBack(USE_OPTIONAL);
         }
@@ -197,26 +164,9 @@ public class SmartDataCodegen extends AbstractJavaCodegen
         if (additionalProperties.containsKey(DATETIME_RELAXED)) {
             dateTimeRelaxed = convertPropertyToBooleanAndWriteBack(DATETIME_RELAXED);
         }
-        if (additionalProperties.containsKey(PAGEABLE)) {
-            pageable = convertPropertyToBooleanAndWriteBack(PAGEABLE);
-        }
-        if (additionalProperties.containsKey(SUPPORT_ASYNC)) {
-            supportAsync = convertPropertyToBooleanAndWriteBack(SUPPORT_ASYNC);
-        }
-        if (additionalProperties.containsKey(GENERATE_API_TESTS)) {
-            generateApiTests = convertPropertyToBooleanAndWriteBack(GENERATE_API_TESTS);
-        }
         if (additionalProperties.containsKey(OPENAPI_NULLABLE)) {
             openApiNullable = convertPropertyToBooleanAndWriteBack(OPENAPI_NULLABLE);
         }
-        if (additionalProperties.containsKey(GENERATE_AUTHENTICATION)) {
-            generateAuthentication = convertPropertyToBooleanAndWriteBack(GENERATE_AUTHENTICATION);
-        }
-        if (additionalProperties.containsKey(GENERATE_EXAMPLES)) {
-            generateExamples = convertPropertyToBooleanAndWriteBack(GENERATE_EXAMPLES);
-        }
-
-        // we do not generate projects, only api, set source and test folder
 
         templateDir = "SmartData";
         projectFolder = getOrDefault.apply("projectFolder", "generated-sources");
@@ -224,26 +174,7 @@ public class SmartDataCodegen extends AbstractJavaCodegen
         sourceFolder = getOrDefault.apply(SOURCE_FOLDER, projectFolder + File.separator + "openapi");
         testFolder = getOrDefault.apply("testFolder", projectTestFolder + File.separator + "openapi");
         modelNameSuffix = getOrDefault.apply(MODEL_NAME_SUFFIX, modelNameSuffix);
-        isClient = additionalProperties.containsKey(CLIENT_ID);
 
-        // add files to generate
-
-        if (isClient) {
-            apiTemplateFiles.put("apiClient.mustache", "Client.java");
-        } else {
-            apiTemplateFiles.put("apiServer.mustache", ".java");
-            if (pageable) {
-                addSupportingFile(sourceFolder, invokerPackage, "PageableProvider");
-            }
-            if (generateApiTests && !openAPI.getPaths().isEmpty()) {
-                apiTestTemplateFiles.put("testSpec.mustache", "Spec.java");
-                apiTestTemplateFiles.put("testClient.mustache", "Client.java");
-                addSupportingFile(testFolder, invokerPackage, "HttpResponseAssertions");
-            }
-        }
-        if (generateExamples) {
-            modelTestTemplateFiles.put("testExampleObject.mustache", "Example.java");
-        }
 
         // add type mappings for mustache
 
@@ -258,158 +189,17 @@ public class SmartDataCodegen extends AbstractJavaCodegen
                 .filter(type -> !type.isBlank())
                 .ifPresent(type -> additionalProperties.put("type.Generated", type));
 
-        this.fieldRenames.putAll(Map.of("@type", "tmForumType", "@schemaLocation", "atSchemaLocation", "@baseType", "atBaseType"));
+        this.fieldNameMapping.putAll(Map.of("@type", "tmForumType", "@schemaLocation", "atSchemaLocation", "@baseType", "atBaseType"));
     }
 
     @Override
     public CodegenOperation fromOperation(String path, String httpMethod, Operation source, List<Server> servers) {
-
-        var operation = super.fromOperation(path, httpMethod, source, servers);
-//        var produces = operation.produces == null ? List.<Map<String, String>>of() : operation.produces;
-//
-//        // warn if operation has wildcard/range responses
-//
-//        if (operation.responses.stream().allMatch(CodegenResponse::isWildcard)) {
-//            throw new SpecValidationException("Only default response is not supported.");
-//        }
-//        if (operation.responses.stream().anyMatch(CodegenResponse::isRange)) {
-//            throw new SpecValidationException("Range responses are not supported.");
-//        }
-//
-//        // get responses for considering repsonse type
-//
-//        var responses = operation.responses.stream().filter(r -> r.is2xx || r.is3xx).collect(Collectors.toList());
-//        var responsesCodes = responses.stream().map(r -> Integer.parseInt(r.code)).collect(Collectors.toList());
-//        var responseGeneric = useGenericResponse;
-//        var dataTypes = responses.stream().map(r -> r.dataType).collect(Collectors.toSet());
-//
-//        if (!responseGeneric && responses.size() > 1) {
-//            LOG.info("operation {} has multiple responses {}, use generic response",
-//                    operation.nickname, responsesCodes);
-//            responseGeneric = true;
-//        }
-//
-//        if (!responseGeneric && responses.stream().anyMatch(r -> r.hasHeaders)) {
-//            LOG.info("operation {} has response with header, use generic response", operation.nickname);
-//            responseGeneric = true;
-//        }
-//
-//        if (operation.returnType != null && dataTypes.size() > 1) {
-//            LOG.info("operation {} has multiple responses {} with multiple models {}, erase return type",
-//                    operation.nickname, responsesCodes, dataTypes);
-//            operation.returnType = null;
-//        }
-//
-//        if ((operation.returnType != null || !responseGeneric) && produces.size() > 1) {
-//            var specResponse = (ApiResponse) responses.get(0).vendorExtensions.get(ApiResponse.class.getName());
-//            var returnTypes = specResponse.getContent().values().stream().collect(Collectors.toSet());
-//            if (returnTypes.size() > 1) {
-//                var mediaTypes = produces.stream().flatMap(m -> m.values().stream()).collect(Collectors.toSet());
-//                LOG.info("operation {} has multiple media types {} with multiple models, erase return type",
-//                        operation.nickname, mediaTypes);
-//                operation.returnType = null;
-//                responseGeneric = true;
-//            }
-//        }
-//
-//        // store method and status for micronaut
-//
-//        var extensions = operation.vendorExtensions;
-//        extensions.put("httpMethod", httpMethod.toUpperCase().charAt(0) + httpMethod.substring(1).toLowerCase());
-//        extensions.put("generic", responseGeneric);
-//        if (responses.size() == 1) {
-//            extensions.put("status", HttpStatus.valueOf(responsesCodes.get(0)).name());
-//        }
-//        operation.responses.forEach(r -> extensions.put("has" + r.code, true));
-//
-//        // jwt provider for tests
-//
-//        var hasSecurityJwt = (boolean) operation.vendorExtensions.getOrDefault("has401", false);
-//        if (generateApiTests && hasSecurityJwt) {
-//            addSupportingFile(testFolder, invokerPackage, "JwtProvider");
-//            addSupportingFile(testFolder, invokerPackage, "JwtBuilder");
-//        }
-//
-//        // async
-//
-//        if (supportAsync) {
-//            var isVoid = operation.returnType == null;
-//            var isStream = Optional.ofNullable(source.getResponses().get("200"))
-//                    .map(ApiResponse::getContent)
-//                    .filter(content -> content.containsKey(MediaType.APPLICATION_JSON_STREAM))
-//                    .isPresent();
-//            extensions.put("asyncContainer", typeMapping.get("asyncSingle"));
-//            extensions.put("asyncStream", isStream);
-//            if (!responseGeneric) {
-//                if (isVoid) {
-//                    var asyncCompletable = typeMapping.get("asyncCompletable");
-//                    if ("reactor.core.publisher.Mono".equals(asyncCompletable)) {
-//                        extensions.put("asyncContainer", asyncCompletable + "<java.lang.Void>");
-//                    } else {
-//                        extensions.put("asyncContainer", asyncCompletable);
-//                    }
-//                } else if ((boolean) operation.vendorExtensions.getOrDefault("has404", false)) {
-//                    extensions.put("asyncContainer", typeMapping.get("asyncMaybe"));
-//                }
-//            }
-//            if (isStream) {
-//                extensions.put("asyncContainer", typeMapping.get("asyncFlowable"));
-//            }
-//        }
-//
-//        // add upper case operationId for path constants
-//
-//        operation.vendorExtensions.put("operationIdUpperCase",
-//                StringUtils.underscore(operation.nickname).toUpperCase());
-//
-//        operation.pathParams.stream().filter(p -> p.defaultValue != null).forEach(p -> {
-//            LOG.warn("operation {} has path param {} with unsupported default value {}, default removed",
-//                    operation.nickname, p.baseName, p.defaultValue);
-//            p.defaultValue = null;
-//        });
-//
-//        // for handle client/server specific path
-//
-//        var clientPath = operation.path;
-//        var queryParamsWithArray = operation.queryParams.stream().filter(p -> p.isArray).collect(Collectors.toList());
-//        if (!queryParamsWithArray.isEmpty()) {
-//            clientPath = operation.path + "?"
-//                    + queryParamsWithArray.stream().map(p -> "{&" + p.baseName + "*}").collect(Collectors.joining());
-//        }
-//        extensions.put("clientPath", clientPath);
-//
-//        // for handle client/server specific path
-//
-//        var serverPath = operation.path;
-//        for (var pathParam : operation.pathParams) {
-//            var name = pathParam.baseName;
-//            if (pathParam.isBoolean) {
-//                serverPath = serverPath.replace("{" + name + "}", "{" + name + ":true|false}");
-//            } else if (pathParam.isInteger || pathParam.isLong) {
-//                serverPath = serverPath.replace("{" + name + "}", "{" + name + ":\\\\-?[0-9]+}");
-//            } else if (pathParam.isUuid) {
-//                serverPath = serverPath.replace("{" + name + "}", "{" + name + ":" + UUID_PATTERN + "}");
-//            } else if (pathParam.maxLength != null) {
-//                serverPath = serverPath.replace("{" + name + "}", "{" + name + ":" + pathParam.maxLength + "}");
-//            } else if (pathParam.pattern != null && !pathParam.pattern.contains("{")) {
-//                serverPath = serverPath.replace("{" + name + "}", "{" + name + ":" + pathParam.pattern + "}");
-//            }
-//        }
-//        extensions.put("serverPath", serverPath);
-
-        return operation;
+        return super.fromOperation(path, httpMethod, source, servers);
     }
 
     @Override
     public CodegenResponse fromResponse(String responseCode, ApiResponse response) {
-        var codegenResponse = super.fromResponse(responseCode, response);
-
-        // save responses with multiple content types
-        // see https://github.com/OpenAPITools/openapi-generator/issues/6708
-
-        codegenResponse.vendorExtensions.put(ApiResponse.class.getName(), response);
-
-        return codegenResponse;
+        return super.fromResponse(responseCode, response);
     }
 
     private boolean isReferenceToEntity(String reference, Map<String, ModelsMap> objs) {
@@ -470,7 +260,9 @@ public class SmartDataCodegen extends AbstractJavaCodegen
     public Map<String, ModelsMap> updateAllModels(Map<String, ModelsMap> objs) {
         var superObjs = super.updateAllModels(objs);
 
-        new SchemaGenerator(outputFolder(), schemaRegistry).writeSchemata(superObjs,fieldRenames);
+        // Create json schema
+        new SchemaGenerator(outputFolder(), schemaRegistry).writeSchemata(superObjs, fieldNameMapping);
+
         addModellingHint(superObjs);
         // remove AllOf
 
@@ -545,21 +337,11 @@ public class SmartDataCodegen extends AbstractJavaCodegen
 
     @Override
     public void postProcessParameter(CodegenParameter parameter) {
-        if (parameter.isFormParam && parameter.isFile) {
-            parameter.dataType = typeMapping.get(supportAsync ? "asyncFileUpload" : "fileUpload");
-        }
-        if (dateTimeRelaxed && (parameter.isDate || parameter.isDateTime)) {
-            addSupportingFile(sourceFolder, invokerPackage, "TimeTypeConverterRegistrar");
-        }
     }
 
     @Override
     public void postProcessModelProperty(CodegenModel model, CodegenProperty property) {
         super.postProcessModelProperty(model, property);
-
-        if (openApiNullable && !property.required && property.isNullable) {
-            property.getVendorExtensions().put("x-jackson-nullable", true);
-        }
     }
 
     @Override
@@ -605,7 +387,6 @@ public class SmartDataCodegen extends AbstractJavaCodegen
 
     @Override
     public void setUseGenericResponse(boolean useGenericResponse) {
-        this.useGenericResponse = useGenericResponse;
     }
 
     @Override
@@ -709,16 +490,5 @@ public class SmartDataCodegen extends AbstractJavaCodegen
 
         // if no example can be generated, leave it null.
         return "null";
-    }
-
-    // internal
-
-    private void addSupportingFile(String folder, String packageString, String file) {
-        var templateFile = "support/" + file + ".mustache";
-        var destinationFilename = folder + "/" + packageString.replace(".", "/") + "/" + file + ".java";
-        var supportingFile = new SupportingFile(templateFile, destinationFilename);
-        if (!supportingFiles.contains(supportingFile)) {
-            supportingFiles.add(supportingFile);
-        }
     }
 }
